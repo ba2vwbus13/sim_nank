0d1b3d8135019b89cd042cffbdc2c9626c891e50
jetbot <ba2vwbus13wind@gmail.com>
2021-01-29 12:22:48 +0900

    add collision

--- scripts/avoidance_controller.py
+++ scripts/avoidance_controller.py
@@ -4,26 +4,26 @@ import cv2
 import numpy as np
 import torch
 import torchvision
+import torchvision.transforms as transforms
 import torch.nn.functional as F
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
 import rospy
 from cv_bridge import CvBridge
 from PIL import Image as PIL_Image
+from conversion_utils import imgmsg_to_pil
 
 class AvoidanceController:
-
-    _bridge = CvBridge()
-    image = None
-    device = torch.device('cuda')
-    mean = 255.0 * np.array([0.485, 0.456, 0.406])
-    stdev = 255.0 * np.array([0.229, 0.224, 0.225])
 
     def __init__(self):
         rospy.Subscriber('/jetbot_camera/raw', Image, self._image_callback)
         self._cmd_vel_pub = rospy.Publisher('/jetbot/avoidance/cmd_vel', Twist, queue_size=1)
         rospy.init_node('avoidance_controller')
         self.model_path = rospy.get_param('~model')
+        self.device = torch.device('cuda')
+        self.mean = torch.Tensor([0.485, 0.456, 0.406]).to(self.device).half()
+        self.stdev = torch.Tensor([0.229, 0.224, 0.225]).to(self.device).half()
+        self.image = None
 
     def start(self):
         rate = rospy.Rate(10)
@@ -49,10 +49,11 @@ class AvoidanceController:
         y = self.model(image)
         y = F.softmax(y, dim=1)
         prob_blocked = float(y.flatten()[0])
+        rospy.loginfo("prob_blocked: {}".format(prob_blocked))
         return prob_blocked > 0.5
 
     def initialize_inferance(self):
-        model = torchvision.models.resnet18(pretraind=False)
+        model = torchvision.models.resnet18(pretrained=False)
         model.fc = torch.nn.Linear(512, 2)
         model.load_state_dict(torch.load(self.model_path))
         model = model.to(self.device)
@@ -60,15 +61,17 @@ class AvoidanceController:
 
     def _preprocess(self):
         image = self.image
-        image = transforms.functional.to_tensor(image).to(device).half()
-        image.sub_(mean[:, None, None]).dev_(std[:, None, None])
+        image = transforms.functional.to_tensor(image).to(self.device).half()
+        image.sub_(self.mean[:, None, None]).div_(self.stdev[:, None, None])
         image = image[None, ...]
         return image
 
 
     def _image_callback(self, msg):
-        image = self._bridge.imgmsg_to_cv2(msg, 'bgr8')
-        self.image = PIL_Image.fromstring("L", cv2.GetSize(image), image.tostring())
+        self.image = imgmsg_to_pil(msg)
+        #self.image = imgmsg_to_pil(msg, 'bgr8')
+        #image = self._bridge.imgmsg_to_cv2(msg, 'bgr8')
+        #self.image = PIL_Image.fromstring("L", cv2.GetSize(image), image.tostring())
 
 
 if __name__ == '__main__':
