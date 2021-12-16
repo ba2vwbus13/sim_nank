c4eff6039f8d075990788364298ca6a54b028eee
nakahira <ba2vwbus13wind@gmail.com>
2021-12-16 14:30:39 +0900

    fix

--- scripts/road_following.py
+++ scripts/road_following.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 #iiyama = python2
 #jetbot = python3
@@ -62,10 +62,9 @@ class RoadFollowingController:
     def mk_image(self):
         image = self.cv_image.copy()
         (height, width, channel) = image.shape
-        print("x:{} y:{}".format(self.xy[0], self.xy[1]))
+        #print("x:{} y:{}".format(self.xy[0], self.xy[1]))
         x = int(width/2*(1+self.xy[0]))
         y = int(height/2*(1+self.xy[1]))
-        print("x:{} y:{}".format(x,y))
         cv2.arrowedLine(image, (int(width/2),int(height/2)), (x,y), (255,255,255), 2, tipLength=0.5)
         image = cv2.resize(image, (int(width), int(height)))
         return image
@@ -87,7 +86,6 @@ class RoadFollowingController:
     def _image_callback(self, msg):
         self.pil_image = imgmsg_to_pil(msg)
         self.pil_image = self.pil_image.resize((224, 224))
-        print(self.pil_image.size)
         if self.display_flip:
             self.pil_image = ImageOps.flip(self.pil_image)
         self.cv_image = pil_to_cv(self.pil_image)
